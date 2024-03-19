0df6990c6032f02222a9f0034a7872891e4a2c95
aahmedtarekk <ahmedtarek8080@gmail.com>
2024-03-20 00:34:52 +0200

    Added addbook function
    Added addBook()

--- Bookstore/src/BuisnessLogic/Controllers/BookController.java
+++ Bookstore/src/BuisnessLogic/Controllers/BookController.java
@@ -85,4 +85,46 @@ public class BookController {
         result = dbConnection.select(Book.class,query);
         return result;
     }
+    public Response addBook(double price, String genre, String title, String author, int quantity, String description) {
+        Response response = new Response();
+        if (genre == null || title == null || author == null || quantity ==0) {  //used == not .equals() to compare references not string values
+            response.status = 402; //custom error for null values
+            response.message = "null arguments are not valid , please try again";
+            return response;
+        }
+        DbConnection dbConnection = new DbConnection();
+        String query = "";
+
+        query = "select * from books where price ="+price + " AND genre='" +genre + "' AND " + "title='" +title  + "' AND " + "author='" + author + "'";
+        System.out.println("query = " + query);
+        ArrayList<Book> result = dbConnection.select(Book.class, query);
+        System.out.println(result.toString());
+        if (!result.isEmpty()) { //custom error status 400 for existing book.
+            System.out.println("Book already exists, quantity will be incremented.");
+            query = "UPDATE books SET quantity=" + (quantity + result.get(0).quantity) + " WHERE title='" + title + "' AND genre='" + genre + "' AND author='" + author + "'";
+            Boolean queryResult = dbConnection.operate(query);
+            if (!queryResult)
+            {
+                response.status = 400;
+                response.message = "Quantity couldn't be incremented";
+                return response;
+            }
+            response.status=200;
+            response.message="Book quantity incremented successfully!";
+            response.object=result;
+            return response;
+        } else {
+            query = "insert into books (price ,genre ,title, author, quantity, description) values ('" + price + "' , '" + genre + "' , '"+title+"' , '" +author + "' , '"+quantity+"', '"+description+"')";
+            Boolean queryResult = dbConnection.operate(query);
+            if (!queryResult) {
+                response.status = 400;
+                response.message = "Error adding this book!";
+                return response;
+            }
+            response.status=200;
+            response.message="Book added successfully.";
+            response.object=result;
+            return response;
+        }
+    }
 }
